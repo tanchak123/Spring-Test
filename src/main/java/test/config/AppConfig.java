c041951d48bb8a4f51f87fe59cae9be6b3835f0f
Andrey Tanchak <tanchak12@gmail.com>
2020-07-14 07:22:22 +0300

    some work with props

--- src/main/java/test/config/AppConfig.java
+++ src/main/java/test/config/AppConfig.java
@@ -3,26 +3,33 @@ package test.config;
 import java.util.Properties;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import test.model.User;
 
 @Configuration
+@PropertySource("classpath:props.properties")
 @ComponentScan(basePackages = {
         "test.dao",
         "test.service"
 })
 public class AppConfig {
 
+    @Autowired
+    private Environment env;
+
     @Bean
     public DataSource getDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost/spring_test?serverTimezone=UTC");
-        dataSource.setUsername("root");
-        dataSource.setPassword("123");
+        dataSource.setDriverClassName(env.getProperty("mysql.driver"));
+        dataSource.setUrl(env.getProperty("mysql.url"));
+        dataSource.setUsername(env.getProperty("mysql.username"));
+        dataSource.setPassword(env.getProperty("mysql.password"));
         return dataSource;
     }
 
@@ -32,9 +39,9 @@ public class AppConfig {
                 = new LocalSessionFactoryBean();
         factoryBean.setDataSource(getDataSource());
         Properties props = new Properties();
-        props.put("hibernate.show_sql", "true");
-        props.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-        props.put("hibernate.hbm2ddl.auto", "create-drop");
+        props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        props.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
         factoryBean.setHibernateProperties(props);
         factoryBean.setAnnotatedClasses(User.class);
         return factoryBean;
