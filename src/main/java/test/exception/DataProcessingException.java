ab3fddc84f2b25a58c7ed08483ef730e00cd12ba
Andrey Tanchak <tanchak12@gmail.com>
2020-07-17 12:15:41 +0300

    added mvc cfg by alishev

--- src/main/java/test/exception/DataProcessingException.java
+++
@@ -1,9 +0,0 @@
-package test.exception;
-
-public class DataProcessingException extends RuntimeException {
-
-    public DataProcessingException(String reason, Throwable e) {
-        super(reason);
-    }
-
-}
